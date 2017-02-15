96a130701363a5eaa1761c34859b0fde07c50074
varad2512 <varad2512@gmail.com>
2017-02-15 23:19:27 +0530

    x

---
+++ geekforgeeks/Dynamic/LIS.py
@@ -0,0 +1,12 @@
+#longest subsequence problem 
+
+def LIS(arr,n):
+	lis = [1]*n
+	for i in range(1,n):
+		for j in range(0,i):
+			if arr[i]>arr[j] and lis[j]+1>lis[i]:
+				lis[i] = lis[j]+1
+	return max(lis)
+
+input = [ 1,2,3,4,5,10, 2, 1]
+print LIS(input,len(input))
