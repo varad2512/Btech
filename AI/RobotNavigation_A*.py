c2203e95898c1993774936234e19043b038a5667
varad2512 <varad2512@gmail.com>
2017-02-17 22:01:01 +0530

    ROBOT A*

--- AI/RobotNavigation_A*.py
+++ AI/RobotNavigation_A*.py
@@ -3,27 +3,29 @@ import numpy as np
 from Queue import *
 import collections
 print "Enter the height:"
-h = int(raw_input())
+height = int(raw_input())
 print "Enter the width:"
-w = int(raw_input())
+width = int(raw_input())
 source = (0,0)
-dest   = (1,1)
-board = np.array([h,w], dtype = 'uint8')
+dest   = (2,0)
+board = np.zeros([height,width], dtype = 'uint8')
+board[source] = 2
+board[dest] = 2
 h = collections.defaultdict()
 g = collections.defaultdict()
 f = collections.defaultdict()
 def show():
-	for x in range(2):
-		for y in range(2):
+	for x in range(height):
+		for y in range(width):
 			print board[x,y],
 		print ""
 
 def hammington(board):
-	for x in range(2):
-		for y in range(2):
-			h[x,y],board[x,y] = (abs(x - destination[0]) + abs(y - destination[1]))
-			g[x,y] = 9999
-			f[x,y] = 9999
+	for x in range(height):
+		for y in range(width):
+			h[(x,y)] = (abs(x - dest[0]) + abs(y - dest[1]))
+			g[(x,y)] = 9999
+			f[(x,y)] = 9999
 def neighbours(pos):
 	return_list = []
 	if pos[0] >= 0  and pos[0] < width and (pos[1]-1) >= 0 and (pos[1]-1) < width:
@@ -36,7 +38,7 @@ def neighbours(pos):
 		return_list.append((pos[0]+1,pos[1]))
 	return return_list
 
-def A*(source,dest):
+def A(source,dest):
 	open_list = PriorityQueue()
 	closed_list = []
 	
@@ -49,8 +51,8 @@ def A*(source,dest):
 		current = open_list.get_nowait()[1]
 		closed_list.append(current)
 		if current == dest:
-			return reconstuctpath(current,camefrom)
-		neighbours_list = neighbours(cuurent)
+			return reconstructpath(current,camefrom)
+		neighbours_list = neighbours(current)
 		for x in neighbours_list:
 			if x in closed_list:
 				continue
@@ -62,8 +64,22 @@ def A*(source,dest):
 			open_list.put_nowait((f[x],x))
 			camefrom[x] = current
 
+def reconstructpath(current, camefrom):
+	if current == source:
+		return [current]
+	else:
+		path = [current]
+		while(True):
+			if current == source:
+				return path
+			else:	
+				current = camefrom[current]
+				path.append(current)
 
-
+hammington(board)
+show()
+result = A(source,dest)
+print result
 
 
 
